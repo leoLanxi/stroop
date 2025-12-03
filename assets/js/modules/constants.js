0ea759adf3e65b5c7ef2cf246f206f224364371c
zlx <zlx@192.168.1.5>
2025-12-03 14:16:34 +0800

    修改游戏时间

--- assets/js/modules/constants.js
+++ assets/js/modules/constants.js
@@ -5,7 +5,8 @@ export const COLORS = {
 
 export const KEY_MAP = { f: "red", d: "yellow", j: "blue", k: "green" };
 
+// 轮数设计为5轮 每轮等待时间在1000ms到2900ms之间随机
 export const ROUNDS = 5;
 export const WAIT_MIN_MS = 1000;
-export const WAIT_MAX_MS = 3900;
+export const WAIT_MAX_MS = 2900;
 
